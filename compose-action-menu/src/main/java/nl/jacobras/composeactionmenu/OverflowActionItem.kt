8d747cae92f9285ed4d302f3f4f6be4c5bb26832
Jacob Ras <jacobras.nl@gmail.com>
2021-12-28 11:41:22 +0100

    Configure library build

--- compose-action-menu/src/main/java/nl/jacobras/composeactionmenu/OverflowActionItem.kt
+++ compose-action-menu/src/main/java/nl/jacobras/composeactionmenu/OverflowActionItem.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nl.jacobras.composeactionmenu.sample.R
 
 @Composable
 internal fun OverflowActionItem(
