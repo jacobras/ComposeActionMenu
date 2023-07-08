7999b6c278e4806ea6018809746a5dd790150f55
Jacob Ras <jacobras.nl@gmail.com>
2023-07-08 11:44:19 +0200

    Update Gradle to 8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "7.4.1" apply false
-    id("com.android.library") version "7.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("com.android.application") version libs.versions.agp apply false
+    id("com.android.library") version libs.versions.agp apply false
+    id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
 }
