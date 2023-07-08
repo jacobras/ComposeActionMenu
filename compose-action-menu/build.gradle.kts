8d4aae042d92af52f5d2cb441f732619bbeab813
Jacob Ras <jacobras.nl@gmail.com>
2023-07-08 11:36:34 +0200

    Migrate library to Kotlin Gradle DSL

---
+++ compose-action-menu/build.gradle.kts
@@ -0,0 +1,53 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.gradle.kotlin.dsl.implementation
+import org.gradle.kotlin.dsl.libs
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    compileSdk = 33
+
+    buildFeatures {
+        compose = true
+    }
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // BoMs
+    implementation(platform(libs.compose.bom))
+
+    implementation(libs.compose.activity)
+    implementation(libs.compose.compiler)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling)
+}
