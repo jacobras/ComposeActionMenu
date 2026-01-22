8e8528d61bf23fd3fe7606e746fcb439fe547a23
Jacob Ras <jacobras.nl@gmail.com>
2026-01-22 19:41:53 +0100

    Migrate web sample to [webMain]

--- sample-web/build.gradle.kts
+++ sample-web/build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.compose.foundation)
+            implementation(libs.compose.icons)
             implementation(libs.compose.material3)
             implementation(libs.compose.resources)
             implementation(libs.compose.runtime)
